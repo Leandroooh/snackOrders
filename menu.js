8f9d280f0b8c8198ce713bc7e8785299e57db415
Leandro <leandrosilvadeoliveira0605@gmail.com>
2024-10-26 17:42:03 -0300

    Order System

---
+++ menu.js
@@ -0,0 +1,30 @@
+export const menuItems = [
+	{
+		name: 'X-Frango',
+		value: 'X-Frango',
+		description:
+			'Pão de Brioche, Alface, Tomate, Filé de Frango e molho da Casa',
+	},
+	{
+		name: 'X-Tudo',
+		value: 'X-Tudo',
+		description:
+			'Pão de Brioche, Alface, Tomate, Filé de Frango, Hamburguer, Tomate, Bacon e molho da Casa',
+	},
+	{
+		name: 'Hamburguer Artesanal',
+		value: 'Hamburguer Artesanal',
+		description:
+			'Pão de Brioche, Alface, Tomate, Hamburguer Artesanal (150g), Tomate e molho da Casa',
+	},
+	{
+		name: 'Suco Natural (Laranja)',
+		value: 'Suco Natural (Laranja)',
+		description: 'Suco de 500ML, sem Açucar',
+	},
+	{
+		name: 'Coca-Cola Zero',
+		value: 'Coca-Cola Zero',
+		description: 'Coca-Cola Lata ( 350ML )',
+	},
+];
